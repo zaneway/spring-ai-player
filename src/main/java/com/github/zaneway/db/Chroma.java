97c480913a108dd0de1de1e31a9df5b5c0ea6af5
zaneway <zaneway1108@gmail.com>
2025-03-30 23:50:38 +0800

    update package

---
+++ src/main/java/com/github/zaneway/db/Chroma.java
@@ -0,0 +1,5 @@
+package com.github.zaneway.db;
+
+public class Chroma {
+
+}
--- src/test/java/com/github/zaneway/db/DemoApplicationTests.java
+++ src/test/java/com/github/zaneway/db/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.github.zaneway.db;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
