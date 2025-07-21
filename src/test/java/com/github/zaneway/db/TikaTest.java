ae0e6ded04aea001b0058ace9a5b34497a1740d1
zaneway <zaneway1108@gmail.com>
2025-07-21 13:24:34 +0800

    add test to read file

--- src/test/java/com/github/zaneway/db/TikaTest.java
+++ src/test/java/com/github/zaneway/db/TikaTest.java
@@ -1,17 +1,57 @@
 package com.github.zaneway.db;
 
-import java.io.File;
-import org.apache.tika.Tika;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.core.io.FileSystemResource;
 
 public class TikaTest {
 
   public static void main(String[] args) throws Exception {
-    Tika tika = new Tika();
-    File file = new File("/Users/zhangzhenwei/Downloads/20250719.docx");
-    String detect = tika.detect(file);
-    //解析成字符串
-    String s = tika.parseToString(file);
-    System.out.println(detect);
+    // 文件路径
+    FileSystemResource from = new FileSystemResource("/Downloads/20250719.docx");
+
+    TikaDocumentReader reader = new TikaDocumentReader(from, ExtractedTextFormatter.builder().withNumberOfTopTextLinesToDelete(90).build());
+    List<Document> documents = reader.get();
+
+//    Pattern chapterPattern = Pattern.compile("^第\\d+章.*"); // 你可以改成 "Chapter \\d+" 或别的格式
+    //数字标题
+    Pattern chapterPattern = Pattern.compile("^\\d+(\\.\\d+)*\\s+.*$");
+
+    XWPFDocument document = new XWPFDocument(
+        new FileInputStream("/Downloads/20250719.docx"));
+    List<XWPFParagraph> paragraphs = document.getParagraphs();
+    ArrayList<String> objects = new ArrayList<>();
+    for (XWPFParagraph para : paragraphs) {
+      String trim = para.getText().trim();
+      if (chapterPattern.matcher(trim).matches()) {
+        String[] s = trim.split(" ");
+        if (s.length > 1) {
+          objects.add(s[1].split("\t")[0]);
+        } else {
+          objects.add(trim.split("\t")[1]);
+        }
+      }
+    }
+    ArrayList<String> strings = new ArrayList<>();
+    String text = documents.get(0).getText();
+    int  end = 0;
+    for (int i = 4; i < objects.size(); i++) {
+      String regex = objects.get(i);
+      end = text.indexOf(regex);
+      String substring = text.substring(0, end);
+      strings.add(substring);
+      text = text.substring(end, text.length());
+    }
+    System.out.println(strings);
+
+
   }
 
 }
