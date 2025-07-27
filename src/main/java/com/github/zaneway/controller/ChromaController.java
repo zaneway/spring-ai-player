fe8e7551512584607369332ad48bf9001b34e0ea
zaneway <zaneway1108@gmail.com>
2025-07-27 21:51:29 +0800

    增加创建databases

--- src/main/java/com/github/zaneway/controller/ChromaController.java
+++ src/main/java/com/github/zaneway/controller/ChromaController.java
@@ -1,16 +1,19 @@
 package com.github.zaneway.controller;
 
+import com.github.zaneway.controller.request.ChromaRequest;
 import jakarta.annotation.Resource;
 import java.util.List;
 import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.ai.chroma.vectorstore.ChromaApi;
 import org.springframework.ai.chroma.vectorstore.ChromaApi.Collection;
 import org.springframework.ai.chroma.vectorstore.ChromaApi.CreateCollectionRequest;
 import org.springframework.ai.chroma.vectorstore.common.ChromaApiConstants;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/chroma")
 public class ChromaController {
 
@@ -23,18 +26,37 @@ public class ChromaController {
     System.out.println(test);
   }
 
-  @RequestMapping("collections/create")
-  public void createCollects() {
-    List<Collection> test = chromaApi.listCollections("zaneway", "test");
-    String defaultTenantName = ChromaApiConstants.DEFAULT_TENANT_NAME;
-    String defaultDatabaseName = ChromaApiConstants.DEFAULT_DATABASE_NAME;
+  @RequestMapping("/databases/create")
+  public String  createDatabases(@RequestBody ChromaRequest request) {
+    if (StringUtils.isEmpty(request.getTenantName())) {
+      request.setTenantName(ChromaApiConstants.DEFAULT_TENANT_NAME);
+    }
+    if (StringUtils.isEmpty(request.getDatabaseName())) {
+      request.setDatabaseName(ChromaApiConstants.DEFAULT_DATABASE_NAME);
+    }
+    try {
+      chromaApi.createDatabase(request.getTenantName(),request.getDatabaseName());
+    } catch (Exception e) {
+      return e.getMessage();
+    }
+    return "success";
+  }
 
+
+  @RequestMapping("collections/create")
+  public String  createCollects(@RequestBody ChromaRequest request) {
+    if (StringUtils.isEmpty(request.getTenantName())) {
+      request.setTenantName(ChromaApiConstants.DEFAULT_TENANT_NAME);
+    }
+    if (StringUtils.isEmpty(request.getDatabaseName())) {
+      request.setDatabaseName(ChromaApiConstants.DEFAULT_DATABASE_NAME);
+    }
     Map<String, Object> metadata = Map.of("hnsw:space", "cosine", "hnsw:M", 20);
-    CreateCollectionRequest createCollectionRequest = new CreateCollectionRequest("TestCreateCollections", metadata);
-    Collection collection = chromaApi.createCollection(defaultTenantName, defaultDatabaseName,
-        createCollectionRequest);
-    String name1 = collection.name();
-    System.out.println(name1);
+    CreateCollectionRequest createCollectionRequest = new CreateCollectionRequest(
+        request.getCollectionsName(), metadata);
+    Collection collection = chromaApi.createCollection(request.getTenantName(),
+        request.getDatabaseName(), createCollectionRequest);
+    return collection.name();
   }
 
 
---
+++ src/main/java/com/github/zaneway/controller/request/ChromaRequest.java
@@ -0,0 +1,15 @@
+package com.github.zaneway.controller.request;
+
+import lombok.Data;
+
+@Data
+public class ChromaRequest {
+
+  private String collectionsName;
+
+  private String tenantName;
+
+  private String databaseName;
+
+
+}
