db1094a3ed09c7b6c810d8f5605d705bca6600ff
zaneway <zaneway1108@gmail.com>
2025-08-04 17:42:25 +0800

    添加markdown文本解析

---
+++ src/main/java/com/github/zaneway/file/MarkdownParseFile.java
@@ -0,0 +1,28 @@
+package com.github.zaneway.file;
+
+import java.util.List;
+import java.util.Map;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.markdown.MarkdownDocumentReader;
+import org.springframework.ai.reader.markdown.config.MarkdownDocumentReaderConfig;
+import org.springframework.ai.reader.markdown.config.MarkdownDocumentReaderConfig.Builder;
+import org.springframework.core.io.Resource;
+
+public class MarkdownParseFile implements ParseFile {
+
+  @Override
+  public List<Document> parseFile(Resource resource, Map<String, Object> additionalMetadata) {
+
+    Builder builder = MarkdownDocumentReaderConfig.builder()
+        .withHorizontalRuleCreateDocument(true)
+        .withIncludeCodeBlock(false)
+        .withIncludeBlockquote(false);
+    if (additionalMetadata != null) {
+      //withAdditionalMetadata方法不要调整顺序,注意内部实现
+      builder.withAdditionalMetadata(additionalMetadata);
+    }
+    builder.withAdditionalMetadata("filename", resource.getFilename());
+    MarkdownDocumentReader reader = new MarkdownDocumentReader(resource, builder.build());
+    return reader.get();
+  }
+}
---
+++ src/main/java/com/github/zaneway/file/ParseFile.java
@@ -0,0 +1,13 @@
+package com.github.zaneway.file;
+
+import java.util.List;
+import java.util.Map;
+import org.springframework.ai.document.Document;
+import org.springframework.core.io.Resource;
+
+public interface ParseFile {
+
+
+  List<Document> parseFile(Resource resource, Map<String, Object> additionalMetadata);
+
+}
