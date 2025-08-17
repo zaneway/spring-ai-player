019a775fc171a0fda317a2e7bd8dfb7baa0d1a8a
zaneway <zaneway1108@gmail.com>
2025-08-17 20:11:42 +0800

    add rag advisor

--- src/main/java/com/github/zaneway/ollama/ChatOllama.java
+++ src/main/java/com/github/zaneway/ollama/ChatOllama.java
@@ -6,6 +6,10 @@ import org.springframework.ai.chat.client.ChatClient.Builder;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.OllamaEmbeddingModel;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,11 +18,20 @@ public class ChatOllama {
   @Resource
   private OllamaChatModel ollamaChatModel;
 
+  @Resource
+  private OllamaEmbeddingModel ollamaEmbeddingModel;
+  @Resource
+  private VectorStore vectorStore;
   public String chat(String msg){
     Builder builder = ChatClient.builder(ollamaChatModel);
     MessageWindowChatMemory chatMemory = MessageWindowChatMemory.builder().maxMessages(1000).build();
-    //todo 可以基于Advisor制作 RAG
-    builder.defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build());
+    //实现rag
+    VectorStoreDocumentRetriever.Builder retriever = VectorStoreDocumentRetriever.builder()
+        .vectorStore(vectorStore).similarityThreshold(0.5).topK(10);
+    RetrievalAugmentationAdvisor retrievalAugmentationAdvisor = RetrievalAugmentationAdvisor.builder()
+        .documentRetriever(retriever.build()).build();
+
+    builder.defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build(),retrievalAugmentationAdvisor);
     ChatClient client = builder.build();
     return client.prompt(msg).call().content();
   }
