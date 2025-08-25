6357bb2c1e23f17b15e578635902158ec90eb3bb
zaneway <zaneway1108@gmail.com>
2025-08-25 21:33:39 +0800

    完善rag实现,入参格式转换

---
+++ src/main/java/com/github/zaneway/controller/request/rag/Data.java
@@ -0,0 +1,12 @@
+package com.github.zaneway.controller.request.rag;
+
+@lombok.Data
+public class Data {
+
+  private String text;
+
+  private String chapter;
+
+  private String overview;
+
+}
---
+++ src/main/java/com/github/zaneway/controller/request/rag/MetaDataRequest.java
@@ -0,0 +1,40 @@
+package com.github.zaneway.controller.request.rag;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author zhangzhenwei
+ * @date 2025/8/22 16:09
+ * @desc
+ *  入参格式
+ * {
+ * 	"id":"uuid",
+ *   "title": "GMT-0034-2014 基于SM2密码算法的证书认证系统密码及其相关安全技术规范",
+ *   "date": "2014",
+ *   "tags": ["SM2", "PKI", "国密标准"],
+ * 	"metadata":{
+ * 			"text":"文本内容",
+ * 			"chapter": "第1章 总则"
+ *            }
+ *  }
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class MetaDataRequest {
+
+  private String title;
+
+  private String date;
+
+  private String[] tags;
+
+  private List<Data> datas;
+
+
+}
